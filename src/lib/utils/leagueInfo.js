e4da54468779db9ffd092220b72bfa0a93734b64
HunterMcCallister <huntermccallister@gmail.com>
2024-08-12 19:26:12 -0600

    Update leagueInfo.js
    Adding Photo

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -33,7 +33,7 @@ export const managers = [
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Boise", // (optional)
       "bio": "Vice Mish. I keep people invested and make stupid projects.",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/Bushyy.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 1995, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "lar", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
